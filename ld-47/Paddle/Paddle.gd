0edc83eeb02186cd4ddc3414254d4af9c6a77174
leogeier <leonard.geier@hotmail.com>
2020-10-03 22:56:44 +0200

    add paddle anim

--- ld-47/Paddle/Paddle.gd
+++ ld-47/Paddle/Paddle.gd
@@ -3,8 +3,16 @@ extends KinematicBody2D
 export var max_speed = 700;
 export var decrease_damp = 0.8
 export var increase_damp = 1.2
+export var paddle_throw_time = 0.2
+
+var icon_pos_y setget set_icon_pos_y
 
 var velocity = Vector2()
+var throw_offset = -20
+var icon_orig_y
+
+func set_icon_pos_y(val):
+	$icon.position.y = val
 
 func get_reflection_dir(_dir):
 	var dir = Vector2(0, 1).rotated($DirArrow.rotation)
@@ -17,12 +25,19 @@ func get_damp():
 	else:
 		return increase_damp
 
-func _ready():
+func throw_timer_end():
 	pass
+
+func _ready():
+	$ThrowTimer.connect("timeout", self, "throw_timer_end")
+	icon_orig_y = $icon.position.y
+	$ThrowTimer.wait_time = paddle_throw_time
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("fire"):
 		$ThrowTimer.start();
+		$ReturnTween.interpolate_property(self, "icon_pos_y", icon_orig_y + throw_offset, icon_orig_y, paddle_throw_time, Tween.TRANS_CIRC, Tween.EASE_OUT)
+		$ReturnTween.start()
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = max_speed
