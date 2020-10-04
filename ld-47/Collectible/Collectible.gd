0625a40a3798820f418b3e7894e619677336aacc
leogeier <leonard.geier@hotmail.com>
2020-10-04 12:56:40 +0200

    Collectibles do not appear in balls

--- ld-47/Collectible/Collectible.gd
+++ ld-47/Collectible/Collectible.gd
@@ -2,11 +2,12 @@ extends Area2D
 
 var value
 var possible_values = [20, 50, 100]
+var is_collectable = false
 
 signal collected
 
 func on_collected(ball):
-	if !ball.is_in_group("ball"):
+	if !ball.is_in_group("ball") or !is_collectable:
 		return
 	
 	ScoreTracker.add_score(value)
@@ -18,4 +19,15 @@ func _ready():
 	randomize()
 	value = possible_values[randi() % possible_values.size()]
 	self.connect("body_entered", self, "on_collected")
+
+func _process(delta):
+	if !is_collectable:
+		is_collectable = true
+		visible = true
+		for body in self.get_overlapping_bodies():
+			if body.is_in_group("ball"):
+				is_collectable = false
+				visible = false
+				break
+		
 
